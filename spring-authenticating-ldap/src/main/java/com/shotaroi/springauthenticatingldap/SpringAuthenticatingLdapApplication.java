ee028ec6ec3766ae30ba1262a6958ff42d9660d7
shotaroi <58347058+shotaroi@users.noreply.github.com>
2025-01-18 00:11:49 +0100

    Create project

---
+++ spring-authenticating-ldap/src/main/java/com/shotaroi/springauthenticatingldap/SpringAuthenticatingLdapApplication.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springauthenticatingldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAuthenticatingLdapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAuthenticatingLdapApplication.class, args);
+    }
+
+}
---
+++ spring-authenticating-ldap/src/test/java/com/shotaroi/springauthenticatingldap/SpringAuthenticatingLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shotaroi.springauthenticatingldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAuthenticatingLdapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
